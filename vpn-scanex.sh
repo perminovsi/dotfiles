913321e4bbbb4cc077a1261c18d308fa92e0867d
perminov <perminovsi@ya.ru>
2021-11-26 20:03:04 +0500

    Update scripts

--- vpn-scanex.sh
+++ vpn-scanex.sh
@@ -4,12 +4,16 @@ then
     nmcli c up "Scanex Rum"
     gsettings set org.gnome.system.proxy mode 'manual'
     # echo "scanex-vpn on"
+    cp ~/.gitconfig.scanex ~/.gitconfig
+    cp ~/.ssh/config.scanex ~/.ssh/config
 elif [ "$1" = "off" ]
 then
     nmcli c down "Scanex Rum"
     gsettings set org.gnome.system.proxy mode 'none'
     nmcli radio wifi off
     nmcli radio wifi on
+    cp ~/.gitconfig.main ~/.gitconfig
+    cp ~/.ssh/config.main ~/.ssh/config
     # echo "scanex-vpn off"
 else
     echo "Usage: vpn-scanex on | off"
--- wired-scanex.sh
+++ wired-scanex.sh
@@ -5,11 +5,15 @@ then
     nmcli c up "Scanex Wired"
     gsettings set org.gnome.system.proxy mode 'manual'
     # echo "wired-scanex on"
+    cp ~/.gitconfig.scanex ~/.gitconfig
+    cp ~/.ssh/config.scanex ~/.ssh/config
 elif [ "$1" = "off" ]
 then
     nmcli c down "Scanex Wired"
     gsettings set org.gnome.system.proxy mode 'none'
     nmcli radio wifi on
+    cp ~/.gitconfig.main ~/.gitconfig
+    cp ~/.ssh/config.main ~/.ssh/config
     # echo "wired-scanex off"
 else
     echo "Usage: wired-scanex on | off"
