3fda546283c691afdb37dfbffe42107eefd1e794
perminov <perminovsi@ya.ru>
2021-11-29 19:16:42 +0500

    fix: wrong slink to gitconfig

--- scripts/vpn-scanex.sh
+++ scripts/vpn-scanex.sh
@@ -3,7 +3,7 @@ if [ "$1" = "on" ]
 then
     nmcli c up "Scanex Rum"
     gsettings set org.gnome.system.proxy mode 'manual'
-    ln -sfv $HOME/.dotfiles/git/gitconfig.scanex $HOME/.gitconfig
+    ln -sfv $HOME/.dotfiles/git/.gitconfig.scanex $HOME/.gitconfig
     ln -sfv $HOME/.dotfiles/ssh/config.scanex $HOME/.ssh/config
 elif [ "$1" = "off" ]
 then
@@ -11,7 +11,7 @@ then
     gsettings set org.gnome.system.proxy mode 'none'
     nmcli radio wifi off
     nmcli radio wifi on
-    ln -sfv $HOME/.dotfiles/git/gitconfig $HOME/.gitconfig
+    ln -sfv $HOME/.dotfiles/git/.gitconfig $HOME/.gitconfig
     ln -sfv $HOME/.dotfiles/ssh/config $HOME/.ssh/config
 else
     echo "Usage: vpn-scanex on | off"
--- scripts/wired-scanex.sh
+++ scripts/wired-scanex.sh
@@ -4,14 +4,14 @@ then
     nmcli radio wifi off
     nmcli c up "Scanex Wired"
     gsettings set org.gnome.system.proxy mode 'manual'
-    ln -sfv $HOME/.dotfiles/git/gitconfig.scanex $HOME/.gitconfig
+    ln -sfv $HOME/.dotfiles/git/.gitconfig.scanex $HOME/.gitconfig
     ln -sfv $HOME/.dotfiles/ssh/config.scanex $HOME/.ssh/config
 elif [ "$1" = "off" ]
 then
     nmcli c down "Scanex Wired"
     gsettings set org.gnome.system.proxy mode 'none'
     nmcli radio wifi on
-    ln -sfv $HOME/.dotfiles/git/gitconfig $HOME/.gitconfig
+    ln -sfv $HOME/.dotfiles/git/.gitconfig $HOME/.gitconfig
     ln -sfv $HOME/.dotfiles/ssh/config $HOME/.ssh/config
 else
     echo "Usage: wired-scanex on | off"
